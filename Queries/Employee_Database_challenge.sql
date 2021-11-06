4973bd0878c77d78611d7e7d661cd21d890aac9c
Petersen <Petersen.Derek@mayo.edu>
2021-11-06 14:06:53 -0500

    Module 7 & Challenge

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,77 @@
+-------------------------------------------------------------------------------------------
+--DELIVERABLE 1 BELOW
+-------------------------------------------------------------------------------------------
+---------------------------------------------
+--retirement_titles creation
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees e
+INNER JOIN titles t ON (t.emp_no=e.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no
+---------------------------------------------
+-- unique_titles creation
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+---------------------------------------------
+-- retiring_titles creation
+SELECT count(title),
+	title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count(title) desc
+---------------------------------------------
+
+-------------------------------------------------------------------------------------------
+--DELIVERABLE 2 BELOW
+-------------------------------------------------------------------------------------------
+---------------------------------------------
+SELECT
+	DISTINCT ON (e.emp_no) 
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees e
+INNER JOIN dept_emp de ON (de.emp_no=e.emp_no)
+INNER JOIN titles t ON (t.emp_no=e.emp_no)
+WHERE de.to_date = '9999-01-01'
+	AND
+	(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no, t.to_date DESC
+---------------------------------------------
+---------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
